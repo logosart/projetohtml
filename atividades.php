f8cef518499be669035d6d36d2b213c23d445fc2
PyGodoy <godoyvitorio99@gmail.com>
2022-11-25 19:51:56 -0300

    alterações html e css

--- atividades.php
+++ atividades.php
@@ -12,15 +12,16 @@
 </head>
 <body class="fundo" background="background.jpg">
     <div class="background"></div>
+    <img class="backativi" src="backativi.png"><img>
     <title>Atividades</title>
     <div class="barra"></div>
-    <a href="home.html"><button class="linkhome">Home</button></a>
+    <a href="home.php"><button class="linkhome">Home</button></a>
     <a href="atividades.php"><button class="linkatividades">Atividades</button></a>
     <a href="notas.php"><button class="linknotas">Notas</button></a>
     <a href="perfil.php"><button class="linkperfil">Perfil</button></a>
-    <a href="calendario.html"><button class="linkcalendario">Calendario Acadêmico</button></a>
-    <a href="horario.html"><button class="linkhorario">Horário de Aula</button></a>
-    <a href="manual.html"><button class="linkmanual">Manual do Aluno</button></a>
+    <a href="calendario.php"><button class="linkcalendario">Calendario Acadêmico</button></a>
+    <a href="horario.php"><button class="linkhorario">Horário de Aula</button></a>
+    <a href="manual.php"><button class="linkmanual">Manual do Aluno</button></a>
     <div class="cabecalho"></div>
     <h1 class="titulo2">Atividades</h1>
     <h1 class="nomealuno"><?=$_SESSION['usuario'][0]?></h1>
